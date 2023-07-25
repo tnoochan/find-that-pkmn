030c348037f05848507c0e8c50f73dda9c38caad
thomas <noochan.t@northeastern.edu>
2023-07-24 16:22:50 -1000

    initial commit, setup project. Fixes #1

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# 3500 PA05 Project Repo
-
-[PA Write Up](https://markefontenot.notion.site/PA-05-8263d28a81a7473d8372c6579abd6481)
+# Pokemon Memory Game Personal Project
---
+++ src/main/java/ftp/Driver.java
@@ -0,0 +1,16 @@
+package ftp;
+
+/**
+ * Program entry point
+ */
+public class Driver {
+
+  /**
+   * Entry point
+   *
+   * @param args - cmd line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("entry point");
+  }
+}
